a2e4c597abf6588aeca2d8c62dc7ad1497749dc6
Douglas Obara <douglasobara97@gmail.com>
2022-08-20 22:56:25 +0300

    complete assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,30 @@
-class Dog
+# Dog
+# is defined within lib/dog.rb
+# with names
+# #name=
+# writes the name of the dog to an instance variable @name
+# #name
+# reads the name of the dog from an instance variable @name
+# with breeds
+# #breed=
+# writes the breed of the dog to an instance variable @breed
+# #breed
+# reads the breed of the dog from an instance variable @breed
 
+class Dog
+  def name
+    @name
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
 end
